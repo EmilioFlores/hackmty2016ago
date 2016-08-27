b87baa0abe575b454fa7fd260df7e2c2c0ba00c4
Emilio Flores Rivera <Emilio Flores Rivera>
2016-08-27 13:45:08 -0500

    make it better

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.listen(app.get('port'), function() {
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
+
         let event = req.body.entry[0].messaging[i]
         let sender = event.sender.id
         if (event.message && event.message.text) {
@@ -42,7 +43,8 @@ app.post('/webhook/', function (req, res) {
                 sendGenericMessage(sender)
                 continue
             }
-           sendTextMessage(sender, "Text fsidjfisdjfiasj, echo: " + text.substring(0, 200))
+           sendTextMessage(sender, req.body); 
+           //sendTextMessage(sender, "Text fsidjfisdjfiasj, echo: " + text.substring(0, 200))
         }
 	if (event.postback) {
            let text = JSON.stringify(event.postback)
